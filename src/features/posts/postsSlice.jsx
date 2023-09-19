dc5edd8e63912366f3725922dddaca9245682872
ananthanandanan <ananthanandanan@gmail.com>
2023-09-19 23:56:31 +0530

    feat: Add AddPostsForm - Used prepare callback for structured payload - PostAdded action - AddPostForm - used nanoid from redux toolkit

--- src/features/posts/postsSlice.jsx
+++ src/features/posts/postsSlice.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 // The initial state of the posts slice
 
@@ -11,13 +11,30 @@ const initialState = [
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        // push only works here because toolkit uses immer.js under the hood
+        state.push(action.payload);
+      },
+      // prepare is a callback function that lets us define the action payload
+
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        };
+      },
+    },
+  },
 });
 
 // The posts actions
-export const {} = postsSlice.actions;
+export const { postAdded } = postsSlice.actions;
 // The posts selector
 export const selectAllPosts = (state) => state.posts;
-
 // The posts reducer
 export default postsSlice.reducer;
