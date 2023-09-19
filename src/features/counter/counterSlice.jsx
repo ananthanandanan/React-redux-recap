70d23ad6421a32b41af15b441971e7aa260107f7
ananthanandanan <ananthanandanan@gmail.com>
2023-09-19 22:14:29 +0530

    Setup Redux and Counter - Setup Store - Counter Slice - Counter Component

---
+++ src/features/counter/counterSlice.jsx
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// The initial state of the counter slice
+const initialState = {
+  count: 0,
+};
+
+// The counter slice
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+// The counter actions
+export const { increment, decrement } = counterSlice.actions;
+
+// The counter reducer
+export default counterSlice.reducer;
