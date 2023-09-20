f5d0e2f508c87557b0f57504d6967414f8900e52
ananthanandanan <ananthanandanan@gmail.com>
2023-09-20 15:56:04 +0530

    feat: AddPost Async thunk

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { postAdded, addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 function AddPostForm() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
 
   // get all users from the store
   const users = useSelector(selectAllUsers);
@@ -17,16 +18,38 @@ function AddPostForm() {
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
+  // title, content, userId are all required fields
+  // addRequestStatus === 'idle' means that the form is not currently submitting
+  // if addRequestStatus === 'pending', the form is currently submitting and
+  // we don't want to allow the user to submit the form again until the current submission is complete
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
+
   const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId));
-      setTitle("");
-      setContent("");
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(
+          addNewPost({
+            title,
+            body: content,
+            userId,
+          })
+        ).unwrap();
+        // unwrap() returns the actual value of the fulfilled action payload (the post object)
+        // and throws an error if the promise is rejected
+        // This is useful for handling asynchronous errors with a try/catch block around the unwrap() call.
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (error) {
+        console.error("Failed to save the post: ", error);
+      } finally {
+        setAddRequestStatus("idle");
+      }
     }
     console.log("Save Post Clicked");
   };
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const userOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
